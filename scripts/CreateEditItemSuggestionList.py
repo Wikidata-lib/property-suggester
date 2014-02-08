67fa7472c428ffddb9c982233df4ce9f3b61b319
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-02-08 23:19:22 +0100

    coding style adjustments

--- scripts/CreateEditItemSuggestionList.py
+++ scripts/CreateEditItemSuggestionList.py
@@ -11,33 +11,34 @@ pathToWiki = 'http://suggester.wmflabs.org/wiki'
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="this program generates a Edit-Item-Suggestions")
     parser.add_argument("input", help="The CSV input file (wikidata triple)", type=CompressedFileType('r'))
-    parser.add_argument("offset", nargs='?', default="0", help="offset")
-    parser.add_argument("limit", nargs='?', default="1000", help="limit")
+    parser.add_argument("offset", nargs='?', default="0", help="offset", type=int)
+    parser.add_argument("limit", nargs='?', default="1000", help="limit", type=int)
     parser.add_argument("properties", nargs='?', default="", help="comma seperated list of properties to be considered")
     args = parser.parse_args()
 
     itemCount=0
+    pidList = args.properties.split(",")
     
     for title, claims in CsvReader.read_csv(args.input):
         itemCount +=1
         if itemCount%100 == 0:
             print str(itemCount)
-        if itemCount < int(args.offset):
+        if itemCount < args.offset:
             continue
-        if itemCount > int(args.limit):
+        if itemCount > args.limit:
             break
-        if(len(claims) > 1):
-            propertyIds = [ i[0] for i in claims ] #get ids from claims
-            propertieParameters = "%2C%20".join( propertyIds[:-1] )
+        if claims:
+            propertyIds = [i[0] for i in claims] #get ids from claims
+            propertieParameters = "%2C%20".join(propertyIds[:-1])
             url = pathToWiki + '/api.php?action=wbsgetsuggestions&format=json&properties='+propertieParameters+'&limit=50&continue=0&language=en'
             suggestions = json.load(urllib2.urlopen(url))["search"]
             suggestIds = ""
             for suggestion in suggestions:
-                if(float(suggestion["correlation"]) > 0.3):
-                    if args.properties == "" or (str(suggestion["id"][1:]) in args.properties.split(",")):
+                if float(suggestion["correlation"]) > 0.3:
+                    if args.properties == "" or (str(suggestion["id"][1:]) in pidList):
                         suggestIds = suggestIds + " " + str(suggestion["id"]) 
                 else:
-                    if(suggestIds!=""):
+                    if suggestIds != "":
                         print 'http://www.wikidata.org/wiki'+'/Item:' + title + suggestIds
                     break;
 
