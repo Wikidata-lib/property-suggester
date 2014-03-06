e2b0e0d29e54d4f6711f2802c0f8ac1fa9bd7d24
Tobias Gritschacher <tobias.gritschacher@wikimedia.de>
2014-03-06 15:23:49 +0100

    debug

--- tests/phpunit/PropertySuggester/Suggesters/SimplePHPSuggesterTest.php
+++ tests/phpunit/PropertySuggester/Suggesters/SimplePHPSuggesterTest.php
@@ -1,13 +1,4 @@
 <?php
-
-namespace PropertySuggester\Suggesters;
-
-use DatabaseBase;
-use MediaWikiTestCase;
-
-use Wikibase\DataModel\Entity\PropertyId;
-use Wikibase\DataModel\Entity\Item;
-
 /**
  *
  * @covers PropertySuggester\Suggesters\SimplePHPSuggester
@@ -20,32 +11,14 @@ use Wikibase\DataModel\Entity\Item;
  * @group medium
  *
  */
-class SimplePHPSuggesterTest extends MediaWikiTestCase {
-
-	/**
-	 * @var SimplePHPSuggester
-	 */
-	protected $suggester;
-
-
-	private function row( $pid1, $pid2, $count, $probability ) {
-		return array( 'pid1' => $pid1, 'pid2' => $pid2, 'count' => $count, 'probability' => $probability );
-	}
+class SimplePHPSuggesterTest extends \MediaWikiTestCase {
 
 	public function setUp() {
 		parent::setUp();
-
 		$this->tablesUsed[] = 'wbs_propertypairs';
-
-		$this->suggester = new SimplePHPSuggester( $this->db );
-
 	}
 
 	public function testDbExists() {
-		$res = $this->db->select( 'user', array( 'user_id' ) );
-		$res = $this->db->select( 'wb_terms', array( 'term_row_id' ) );
-		$res = $this->db->select( 'wbs_propertypairs', array( 'pid1' ) );
 		$this->assertTrue(true);
 	}
 }
-
