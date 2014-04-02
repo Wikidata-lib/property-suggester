f39dea5f01099096236b11681044a558ca45232a
Virginia Weidhaas <virginia.weidhaas@student.hpi.uni-potsdam.de>
2014-04-02 15:49:40 +0200

    initial commit

--- modules/ext.PropertySuggester.js
+++ modules/ext.PropertySuggester.js
@@ -1,61 +1,77 @@
 
-var selected_ids = [];
 
-function removeFromArray(arr, element) {
-	 arr.splice($.inArray(element, arr),1);
- }
- 
-function deleteFromList(evt){
-	var pid = evt.data;
-	removeFromArray(selected_ids, pid);
-	$(this).closest('li').remove();
-	doQuery();
-	return false;
+var itemId = "";
+
+
+
+        //action=&format=json&language=en&type=item&continue=0&search=d&_=1396365714038
+
+function getPropertiestoEntity()
+{ var entityChooser = $('#entity-chooser');
+    var input_entity = entityChooser.val();
+    console.log(input_entity);
+    var eid = $("#entity-chooser").data("entityselector").selectedEntity().id;
+    itemId = eid;
+    console.log(eid);
+    var url = mw.util.wikiScript( 'api' ) + '?action=wbgetentities&format=json&ids=' +
+   itemId + '&limit=20&language=' + wgPageContentLanguage;
+    $.get(url, function( data ) {
+        $("#result.item").html("");
+        $('#result-item').append('<h3>Chosen Item: '+ input_entity + '</h3>');
+        $('#result-item').append('<h3>Properties:</h3>');
+
+        var prop = data["entities"][eid]["claims"];
+
+        $.each(prop, function (k,v) {
+            //$('#result-item').append(JSON.stringify(k) + '<br>');
+            var prop_url = mw.util.wikiScript( 'api' ) + '?action=wbgetentities&format=json&ids=' +
+                k + '&limit=10&language=' + wgPageContentLanguage;
+            $.get(prop_url, function( data ) {
+                var suggestions = data['entities'];
+                $.each(suggestions, function (k, v) {
+                    $('#result-item').append(JSON.stringify(v["labels"][wgPageContentLanguage]["value"]) + '<br>');
+                });
+            });
+        });
+    });
+    var url2 = mw.util.wikiScript( 'api' ) + '?action=wbsgetsuggestions&format=json&entity=' +
+        itemId + '&limit=7&language=' + wgPageContentLanguage;
+    $.get(url2, function( data ) {
+        $('#result').html("");
+        $('#result').html('<h3>Suggestions:</h3>');
+        var suggestions = data['search'];
+        $.each(suggestions, function (k, v) {
+            var property_id= v["id"];
+            //$('#result').append(JSON.stringify(v["id"]) + ":-) "+'<br>');
+            var prop_url = mw.util.wikiScript( 'api' ) + '?action=wbgetentities&format=json&ids=' +
+                property_id + '&limit=20&language=' + wgPageContentLanguage;
+            $.get(prop_url, function( data ) {
+                var suggestions = data['entities'];
+                $.each(suggestions, function (k, v) {
+                    $('#result').append(JSON.stringify(v["labels"][wgPageContentLanguage]["value"]) + '<br>');
+                });
+            });
+
+
+        });
+    });
+
 }
-
-function handleInput () {
-    var propertyChooser = $( '#property-chooser');
-	var input_text =  propertyChooser.val();
-	var pid = propertyChooser.next('input').val();
-	if (input_text!==  '' && pid !== ''){	
-		selected_ids.push(pid);
-		var delete_link = $('<a href="#"> x </a>').click(pid, deleteFromList);
-		var li_element = $('<li>' + input_text + ' (' + pid + ')' + '</input></li>');
-		li_element.append(delete_link);
-		$('#selected-properties-list').append(li_element);
-		propertyChooser.val('').focus();
-		doQuery();
-	}
-}
-
-function doQuery() {
-	var url = mw.util.wikiScript( 'api' ) + '?action=wbsgetsuggestions&format=json&properties=' +
-			selected_ids.map(encodeURIComponent).join(',') + '&limit=20&language=' + wgPageContentLanguage;
-	$.get(url, function( data ) {
-		$('#result').html('<h3>Suggestions:</h3>');
-		var suggestions = data['search'];
-		$.each(suggestions, function (k, v) {
-			$('#result').append(JSON.stringify(v) + '<br>');
-		});
-	});
-}
-
 $( document ).ready(function (){
-	var propertyChooser = $( '#property-chooser' );
-	propertyChooser.entityselector({
+        var entityChooser = $( '#entity-chooser' );
+	entityChooser.entityselector({
 		url: mw.util.wikiScript( 'api' ),
 		selectOnAutocomplete: true, 
-		type: 'property'
-	});
-	
-	$( '#add-property-btn' ).click(function() {
-		handleInput();
+		type: 'item'
 	});
 
+    $( '#add-property-btn2' ).click(function() {
+        getPropertiestoEntity();
+    });
 
-    propertyChooser.keyup(function (e) {
+    entityChooser.keyup(function (e) {
 		if (e.keyCode === 13) {
-			handleInput();
+			getPropertiestoEntity()
 		}
 	});
 	
--- src/PropertySuggester/SpecialSuggester.php
+++ src/PropertySuggester/SpecialSuggester.php
@@ -28,16 +28,24 @@ class SpecialSuggester extends SpecialPage {
 			. 'Try for example <i>place of birth</i> (person) and <i>singles record</i> (tennis player)'
 			. ' and look how the results match to tennis player and persons.</p>'
 		);
+                $out->addHTML( "<input placeholder='Item' id='entity-chooser' autofocus>" );
+                $out->addHTML("<br/>");
 
-		$out->addHTML( "<input placeholder='Property' id='property-chooser' autofocus>" );
+                
+                
+		//$out->addHTML( "<input placeholder='Property' id='property-chooser' autofocus>" );
+                
 
-		$out->addHTML( "<input type='button' value='Add' id='add-property-btn'> </input>" );
+		//$out->addHTML( "<input type='button' value='Add' id='add-property-btn'> </input>" );
+
+        $out->addHTML( "<input type='button' value='Dummy' id='add-property-btn2'> </input>" );
 
 		$out->addHTML( "<p/>" );
 
 		$out->addHtml( "<ul id='selected-properties-list'></ul>" );
 
 		$out->addHtml( "<p/>" );
+        $out->addHTML( "<div id='result-item'></div>" );
 
 		$out->addHTML( "<div id='result'></div>" );
 	}
