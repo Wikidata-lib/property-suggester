9c49cdfa91a0fff4eac2faf7f3f3e09a66786324
Virginia Weidhaas <virginia.weidhaas@student.hpi.uni-potsdam.de>
2014-04-02 16:33:33 +0200

    remove lines

--- src/PropertySuggester/SpecialSuggester.php
+++ src/PropertySuggester/SpecialSuggester.php
@@ -20,7 +20,6 @@ class SpecialSuggester extends SpecialPage {
 	public function execute( $par ) {
 
 		$out = $this->getContext()->getOutput();
-
 		$this->setHeaders();
 		$out->addModules( 'ext.PropertySuggester' );
 		$out->addWikiMsg( 'propertysuggester-intro' );
@@ -28,25 +27,16 @@ class SpecialSuggester extends SpecialPage {
 			. 'Try for example <i>place of birth</i> (person) and <i>singles record</i> (tennis player)'
 			. ' and look how the results match to tennis player and persons.</p>'
 		);
-                $out->addHTML( "<input placeholder='Item' id='entity-chooser' autofocus>" );
-                $out->addHTML("<br/>");
+        $out->addHTML( "<form action=\"input_text.htm\"><input placeholder='Item' id='entity-chooser' autofocus></form>" );
+        $out->addHTML("<br/>");
 
-                
-                
 		//$out->addHTML( "<input placeholder='Property' id='property-chooser' autofocus>" );
-                
-
 		//$out->addHTML( "<input type='button' value='Add' id='add-property-btn'> </input>" );
-
         $out->addHTML( "<input type='button' value='Dummy' id='add-property-btn2'> </input>" );
-
 		$out->addHTML( "<p/>" );
-
 		$out->addHtml( "<ul id='selected-properties-list'></ul>" );
-
 		$out->addHtml( "<p/>" );
         $out->addHTML( "<div id='result-item'></div>" );
-
 		$out->addHTML( "<div id='result'></div>" );
 	}
 }
